b763271e4f10c3328b6d99a5615c9a92a85c2219
sijirama <gbemilesanmi@gmail.com>
2024-08-19 23:16:55 +0100

    init

--- tests/ping_test.go
+++ tests/ping_test.go
@@ -9,16 +9,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHealthcheckRoute(t *testing.T) {
-	router := api.SetupRouter()
-
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/healthcheck", nil)
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "pong", w.Body.String())
-}
+// func TestHealthcheckRoute(t *testing.T) {
+// 	router := api.SetupRouter()
+//
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest("GET", "/healthcheck", nil)
+// 	router.ServeHTTP(w, req)
+//
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.Equal(t, "OK", w.Body.String())
+// }
 
 func TestPingRoute(t *testing.T) {
 	router := api.SetupRouter()
